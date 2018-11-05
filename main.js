6f3b75b3dea2712261c380b74766461cd48cacbe
hanadrage <43888774+hanadrage@users.noreply.github.com>
2018-11-05 09:28:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cards.push('cardOne');
+console.log("User flipped" + cardOne);
+var cardTwo = cards[2];
+cards.push('cardTwo')
+console.log("User flipped" + cardTwo)
+if (cardOne = cardsInPlay) {
+  alert("You found a match!");
+}
+if (cardsInPlay > cardOne){
+  alert("Sorry! Try again!");
+}
